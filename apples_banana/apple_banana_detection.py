5aabc4fa6781bdba8ce99f8e0144a02e61967f56
prishasarna <156129631+prishasarna@users.noreply.github.com>
2025-06-30 11:09:48 -0600

    Update apple_banana_detection.py

--- apples_banana/apple_banana_detection.py
+++ apples_banana/apple_banana_detection.py
@@ -1,16 +1,28 @@
 import cv2
 import numpy as np
+img1 = cv2.imread('apple1.jpg',0)
+img2 = cv2.imread('apple2.jpg',0)
+img3 = cv2.imread('apple3.jpg',0)
 
-img1 =cv2.imread('apple1.jpg',0)
-img2 =cv2.imread('apple2.jpg',0)
-img3 =cv2.imread('apple3.jpg',0)
-img4 =cv2.imread('apple4.jpg',0)
-img5 =cv2.imread('banana1.jpg',0)
-img6=cv2.imread('banana2.jpg',0)
-img7=cv2.imread('banana3.jpg',0)
-test=cv2.imread('banana4.jpg',0)
-#combine all images into one array
-train =np.concatenate((img1,img2,img3,img4,img5,img6,img7),axis=0)
-train=train.reshape(-1,10000).astype(np.float32)
-test=test.reshape(-1,10000).astype(np.float32)
-#label for training and testing, apple=0 and banana=1
+img4 = cv2.imread('banana1.jpg',0)
+img5 = cv2.imread('banana2.jpg',0)
+img6 = cv2.imread('banana3.jpg',0)
+
+test = cv2.imread('banana5.jpg',0)
+
+# Combine the images into one array
+train = np.concatenate((img1, img2, img3, img4, img5, img6), axis=0)
+
+# Prepare train and test datasets
+train = train.reshape(-1,10000).astype(np.float32)
+test = test.reshape(-1,10000).astype(np.float32)
+#create labels for train /test,Apple->0 and Banana->1
+train_labels = np.array([[0],[0],[0],[1],[1],[1]]).astype(np.float32)
+knn = cv2.ml.KNearest_create()
+knn.train(train,cv2.ml.ROW_SAMPLE,train_labels)
+ret,result,neighbors,dist = knn.findNearest(test,k=1)
+
+if result==0:
+    print("This would be Apple")
+elif result==1:
+    print("This would be Banana")
